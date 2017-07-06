09d9438d2b0e3256b9e0b0cabe9fc08d7bcc707b
Igor Lapin <raynor73@gmail.com>
2017-07-06 10:14:03 +0300

    Implement basic bounding sphere

--- app/src/main/cpp/game/BoundingSphere.h
+++ app/src/main/cpp/game/BoundingSphere.h
@@ -2,8 +2,21 @@
 #define BOUNDINGSPHERE_H
 
 
-class BoundingSphere {
+#include <engine/core/vector3f.h>
 
+class BoundingSphere
+{
+public:
+	BoundingSphere(const Vector3f &center, float radius) :
+			m_center(center),
+			m_radius(radius) {}
+
+	Vector3f center() const { return m_center; }
+	float radius() const { return m_radius; }
+
+private:
+	const Vector3f m_center;
+	const float m_radius;
 };
 
 
